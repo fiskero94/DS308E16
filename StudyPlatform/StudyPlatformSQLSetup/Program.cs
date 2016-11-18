4cf4fa4136af4a464047d2dc6a42b6d246998918
fiskero94 <fiskero94@gmail.com>
2016-11-18 08:48:18 +0100

    SQLSetup Added Again

---
+++ StudyPlatform/StudyPlatformSQLSetup/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace StudyPlatformSQLSetup
+{
+    class Program
+    {
+        static bool success = false;
+        static string rootPassword;
+        static string connectionString { get { return @"server=localhost;user=root;port=3306;password=" + rootPassword + ";"; } }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This executable will setup the 'studyplatform' database on the local MySQL server. ");
+            Console.WriteLine("If a 'studyplatform' database already exists, it will be dropped.");
+            Console.Write("Input MySQL root password: ");
+            rootPassword = Console.ReadLine();
+
+            try
+            {
+                Setup();
+                success = true;
+            }
+            catch (MySqlException e)
+            {
+                WriteErrorMessage("Setup failed with the exception: " + e.Message);
+                try { ExecuteQuery("DROP DATABASE IF EXISTS studyplatform;"); }
+                catch (Exception) { }
+            }
+
+            if (success) Console.WriteLine("Setup completed, press any key to exit");
+            else Console.WriteLine("Setup failed, press any key to exit");
+            Console.ReadKey();
+        }
+        static void Setup()
+        {
+            // Database creation
+            WriteSetupMessage("Dropping old studyplatform database if exists");
+            ExecuteQuery("DROP DATABASE IF EXISTS studyplatform;");
+            WriteSetupMessage("Creating new studyplatform database");
+            ExecuteQuery("CREATE DATABASE studyplatform;");
+            // Table creation
+            WriteSetupMessage("Creating table for users");
+            ExecuteQuery("USE studyplatform;");
+            ExecuteQuery("CREATE TABLE users (username text, firstname text, lastname text, password text, type text);");
+            WriteSetupMessage("Setting up admin user");
+            ExecuteQuery("INSERT INTO users VALUES('admin','N/A','N/A','password','secretary');");
+            // User creation
+            WriteSetupMessage("Dropping old studyplatform user if exists");
+            ExecuteQuery("DROP USER IF EXISTS 'studyplatformuser'@'localhost';");
+            WriteSetupMessage("Creating new MySQL user: studyplatformuser");
+            ExecuteQuery("CREATE USER 'studyplatformuser'@'localhost' IDENTIFIED BY '7JPRFq9LeNet4DU2NNB4rwmP';");
+            WriteSetupMessage("Granting all priviliges on database 'studyplatform' for user 'studyplatformuser'");
+            ExecuteQuery("GRANT ALL PRIVILEGES ON studyplatform . * TO 'studyplatformuser'@'localhost';");
+        }
+        static void ExecuteQuery(string query)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using (MySqlCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText = query;
+                command.ExecuteNonQuery();
+            }
+        }
+        static void WriteSetupMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("> " + message);
+            Console.ResetColor();
+        }
+        static void WriteErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
