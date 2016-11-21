d938d0f7c2efccc10b540574ef9e3aafaa22d6bf
Rasmus Thomsen <Rasmus Thomsen@RASMUSTHOMSEN>
2016-11-21 11:58:49 +0100

    News class

--- StudyPlatform/StudyPlatform/Classes/Model/News.cs
+++ StudyPlatform/StudyPlatform/Classes/Model/News.cs
@@ -7,5 +7,65 @@ namespace StudyPlatform.Classes.Model
 {
     public class News
     {
+        private uint _id;
+        public uint ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+        private uint _authorid;
+        public uint AuthorID
+        {
+            get
+            {
+                return _authorid;
+            }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                else
+                {
+                    Editor.SetValue("news", ID, "title", value);
+                    _title = value;
+                }
+            }
+        }
+        private string _text;
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                else
+                {
+                    Editor.SetValue("news", ID, "text", value);
+                    _text = value;
+                }
+            }
+        }
+        
+
+        /*
+         HALLOOOOOOOOOOOOO Mangler date - convert fra mysql til datetime
+        */
+
     }
+
 }
