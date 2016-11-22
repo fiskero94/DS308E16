1316f191e3f022e1158c26c2dcfbea9e77bb91ff
hype77 <bjma15@student.aau.dk>
2016-11-22 11:21:27 +0100

    added removeperson

--- StudyPlatform/StudyPlatform/Classes/Database/Remover.cs
+++ StudyPlatform/StudyPlatform/Classes/Database/Remover.cs
@@ -7,7 +7,39 @@ namespace StudyPlatform.Classes.Database
     {
         public static void RemovePerson(Person person)
         {
-            throw new NotImplementedException();
+            Commands.DeleteFrom("persons", "id=" + person.ID);
+            Commands.DropTable("personsentmessages" + person.ID);
+            Commands.DropTable("personrecievedmessages" + person.ID);
+
+            foreach (Message message in Lists.Messages)
+            {
+                Commands.DeleteFrom("messagerecipients" + message.ID, "personid=" + person.ID);
+            }
+
+            if (person is Student)
+            {
+                foreach (Course course in Lists.Courses)
+                {
+                    Commands.DeleteFrom("coursestudents" + course.ID, "studentid=" + person.ID);
+                }
+                foreach (Lesson lesson in Lists.lessons)
+                {
+                    Commands.DeleteFrom("lessonabsences" + lesson.ID, "studentid=" + person.ID);
+                }
+                Commands.DropTable("personcourses" + person.ID);
+                Commands.DropTable("personassignments" + person.ID);
+                Commands.DropTable("personabsences" + person.ID);
+            }
+            if (person is Teacher)
+            {
+                foreach (Course course in Lists.Courses)
+                {
+                    Commands.DeleteFrom("courseteachers" + course.ID, "teacherid=" + person.ID);
+                }
+                Commands.DropTable("personcourses" + person.ID);
+            }
+            person = null;
+
         }
         public static void RemoveMessage(Message message)
         {
@@ -16,6 +48,7 @@ namespace StudyPlatform.Classes.Database
         public static void RemoveNews(News news)
         {
             Commands.DeleteFrom("news", "id=" + news.ID);
+            news = null;
         }
         public static void RemoveCourse(Course course)
         {
@@ -43,11 +76,30 @@ namespace StudyPlatform.Classes.Database
             foreach (AssignmentDescription assigmentdescription in Lists.AssignmentDescriptions)
                 if (true)
                     Commands.DeleteFrom("assignments", "assignmentdescriptionid=" + assigmentdescription.ID);
+            course = null;
         }
         public static void RemoveLesson(Lesson lesson)
         {
+            //Deleting the different tabels lessonrooms, lessonabsences and lessondocuments.
+            Commands.DropTable("lessonrooms" + lesson.ID);
+            Commands.DropTable("lessonabsences" + lesson.ID);
+            Commands.DropTable("lessondocuments" + lesson.ID);
+
+            //deleting the ID from lessons.
             Commands.DeleteFrom("lessons", "id=" + lesson.ID);
 
+            // Deleting the lesson.ID from different classes/tabels.
+            foreach (Person person in Lists.Persons)
+                Commands.DeleteFrom("personabsences" + person.ID, "lessonid=" + lesson.ID);
+            foreach (Room room in Lists.Rooms)
+            {
+                Commands.DeleteFrom("roomreservations" + room.ID, "lessonid=" + lesson.ID);
+            }
+            foreach (Course course in Lists.Courses)
+            {
+                Commands.DeleteFrom("courselessons" + course.ID, "lessonid=" + lesson.ID);
+            }
+            lesson = null;
         }
         public static void RemoveRoom(Room room)
         {
