77951ae0c030bfb10bdced0184684b198f3c41a6
Stg3orge <jsth15@student.aau.dk>
2016-11-22 10:06:50 +0100

    Updated AssignmentDescription Remover

--- StudyPlatform/StudyPlatform/Classes/Database/Remover.cs
+++ StudyPlatform/StudyPlatform/Classes/Database/Remover.cs
@@ -60,10 +60,14 @@ namespace StudyPlatform.Classes.Database
             Commands.DeleteFrom("assignmentdescriptions", "id=" + assignmentDescription.ID);
             Commands.DropTable("assignmentdescriptionassignments" + assignmentDescription.ID);
             Commands.DropTable("assignmentdescriptiondocuments" + assignmentDescription.ID);
-            foreach(Assignment assignment in Lists.)
+
+
+
+            foreach (Assignment assignment in Lists.Assignments)
             {
-                Commands.DeleteFrom()
+                Commands.DeleteFrom("assignments", "assignmentid=" + assignment.ID);
             }
+
             throw new NotImplementedException();
         }
         public static void RemoveAssignment(Assignment assignment)
