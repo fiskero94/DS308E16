a0d2503afe9a98364fb3a68340fdb34a54967cf7
stg3orge <cardisbunx@gmail.com>
2016-11-23 11:31:56 +0100

    ListsTests

--- StudyPlatform/StudyPlatform.Tests/DatabaseTests/ListsTests.cs
+++ StudyPlatform/StudyPlatform.Tests/DatabaseTests/ListsTests.cs
@@ -2,6 +2,9 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyPlatform.Classes.Database;
+using StudyPlatform.Classes.Model;
+using System.Linq;
 
 namespace StudyPlatform.Tests.ModelTests
 {
@@ -14,6 +17,39 @@ namespace StudyPlatform.Tests.ModelTests
             //
             // TODO: Add test logic here
             //
+        }
+
+        [TestMethod]
+        public void ListsPersons_ListParametersFilled_ContainsPersonDataFromDatabase()
+        {
+            // Arrange
+            List<Person> persons = new List<Person>();
+            Query query = new Query("SELECT * FROM studyplatform.persons;");
+            persons = Extractor.ExtractPersons(query.Execute());
+            uint id = Instances.ID;
+            string name = Instances.Name;
+            string type = "student";
+
+            // Act
+            
+        }
+
+        [TestMethod]
+        public void ListsStudents_ListParametersFilled_ContainsStudentData()
+        {
+            // Arrange
+            Creator.CreateStudent(Instances.Name, Instances.Username, Instances.Password);
+            List<Student> students = Lists.Students;
+
+            uint id = Instances.ID;
+            string name = Instances.Name;
+            Student student = new Student(id, name);
+            List<Student> expected = new List<Student>();
+
+            expected.Add(student);
+
+            // Act
+            Assert.AreEqual(expected, students);
         }
     }
 }
