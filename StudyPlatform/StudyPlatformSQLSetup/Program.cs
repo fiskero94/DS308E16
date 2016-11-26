5006c1d67261c27b80b640eb9d6f613d02bffb1b
fiskero94 <fiskero94@gmail.com>
2016-11-26 14:47:38 +0100

    SQLSetup Formatting

--- StudyPlatform/StudyPlatformSQLSetup/Program.cs
+++ StudyPlatform/StudyPlatformSQLSetup/Program.cs
@@ -8,16 +8,16 @@ namespace StudyPlatformSQLSetup
 {
     public class Program
     {
-        static string rootPassword;
-        static string connectionString;
-        static void Main(string[] args)
+        private static string _rootPassword;
+        private static string _connectionString;
+        private static void Main(string[] args)
         {
             
             Console.WriteLine("This executable will setup the 'studyplatform' database on the local MySQL server. ");
             Console.WriteLine("If a 'studyplatform' database already exists, it will be dropped.");
             Console.Write("Input MySQL root password: ");
-            rootPassword = Console.ReadLine();
-            connectionString = @"server=localhost;user=root;port=3306;password=" + rootPassword + ";";
+            _rootPassword = Console.ReadLine();
+            _connectionString = @"server=localhost;user=root;port=3306;password=" + _rootPassword + ";";
 
             bool success = false;
             try
@@ -35,11 +35,10 @@ namespace StudyPlatformSQLSetup
                 catch (Exception) { }
             }
 
-            if (success) Console.WriteLine("Setup completed, press any key to exit");
-            else Console.WriteLine("Setup failed, press any key to exit");
+            Console.WriteLine(success ? "Setup completed, press any key to exit" : "Setup failed, press any key to exit");
             Console.ReadKey();
         }
-        static void SetupDatabase()
+        private static void SetupDatabase()
         {
             WriteSetupMessage("Dropping old studyplatform database if exists");
             ExecuteQuery("DROP DATABASE IF EXISTS studyplatform;");
@@ -47,7 +46,7 @@ namespace StudyPlatformSQLSetup
             ExecuteQuery("CREATE DATABASE studyplatform;");
             ExecuteQuery("USE studyplatform;");
         }
-        static void SetupMySqlUser()
+        private static void SetupMySqlUser()
         {
             WriteSetupMessage("Dropping old studyplatformuser if exists");
             ExecuteQuery("DROP USER IF EXISTS 'studyplatformuser'@'localhost';");
@@ -59,42 +58,52 @@ namespace StudyPlatformSQLSetup
         public static void SetupTables(string externalConnectionString)
         {
             if (externalConnectionString != null)
-                connectionString = externalConnectionString;
+                _connectionString = externalConnectionString;
 
-            CreateTable("Person", "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
-                                   "Name TEXT NOT NULL",
-                                   "Username TEXT NOT NULL",
-                                   "Password TEXT NOT NULL",
-                                   "Type ENUM('Student','Teacher','Secretary') NOT NULL");
-            CreateTable("PersonCourse", "PersonID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
-                                   "CourseID INT UNSIGNED NOT NULL");
-            CreateTable("StudentAbsence", "StudentID INT UNSIGNED NOT NULL",
-                                   "LessonID INT UNSIGNED NOT NULL");
-            CreateTable("Message", "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
-                                    "SenderID INT UNSIGNED NOT NULL",
-                                    "Title TEXT NOT NULL",
-                                    "Text TEXT NOT NULL",
-                                    "DateTime DATETIME NOT NULL");
-            CreateTable("MessageRecipient", "MessageID INT UNSIGNED NOT NULL",
-                                   "PersonID INT UNSIGNED NOT NULL");
-            CreateTable("MessageFile", "MessageID INT UNSIGNED NOT NULL",
-                                   "Filepath TEXT NOT NULL");
-            CreateTable("News", "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
-                                "AuthorID INT UNSIGNED NOT NULL",
-                                "Title TEXT NOT NULL",
-                                "Text TEXT NOT NULL",
-                                "DateTime DATETIME NOT NULL");
-            CreateTable("Course", "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
-                                   "Name TEXT NOT NULL",
-                                   "Description TEXT NOT NULL");
-            CreateTable("CourseFile", "CourseID INT UNSIGNED NOT NULL",
-                                   "Filepath TEXT NOT NULL");
-            CreateTable("Lesson", "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
-                                   "CourseID INT UNSIGNED NOT NULL",
-                                   "Description TEXT NOT NULL",
-                                   "Online BOOL NOT NULL",
-                                   "Cancelled BOOL NOT NULL",
-                                   "DateTime DATETIME NOT NULL");
+            CreateTable("Person", 
+                "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
+                "Name TEXT NOT NULL",
+                "Username TEXT NOT NULL",
+                "Password TEXT NOT NULL",
+                "Type ENUM('Student','Teacher','Secretary') NOT NULL");
+            CreateTable("PersonCourse", 
+                "PersonID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY", 
+                "CourseID INT UNSIGNED NOT NULL");
+            CreateTable("StudentAbsence", 
+                "StudentID INT UNSIGNED NOT NULL",
+                "LessonID INT UNSIGNED NOT NULL");
+            CreateTable("Message", 
+                "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
+                "SenderID INT UNSIGNED NOT NULL",
+                "Title TEXT NOT NULL",
+                "Text TEXT NOT NULL",
+                "DateTime DATETIME NOT NULL");
+            CreateTable("MessageRecipient", 
+                "MessageID INT UNSIGNED NOT NULL",
+                "PersonID INT UNSIGNED NOT NULL");
+            CreateTable("MessageFile", 
+                "MessageID INT UNSIGNED NOT NULL",
+                "Filepath TEXT NOT NULL");
+            CreateTable("News", 
+                "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
+                "AuthorID INT UNSIGNED NOT NULL",
+                "Title TEXT NOT NULL",
+                "Text TEXT NOT NULL",
+                "DateTime DATETIME NOT NULL");
+            CreateTable("Course", 
+                "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
+                "Name TEXT NOT NULL",
+                "Description TEXT NOT NULL");
+            CreateTable("CourseFile", 
+                "CourseID INT UNSIGNED NOT NULL",
+                "Filepath TEXT NOT NULL");
+            CreateTable("Lesson", 
+                "ID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY",
+                "CourseID INT UNSIGNED NOT NULL",
+                "Description TEXT NOT NULL",
+                "Online BOOL NOT NULL",
+                "Cancelled BOOL NOT NULL",
+                "DateTime DATETIME NOT NULL");
             CreateTable("LessonRoom",
                 "LessonID INT UNSIGNED NOT NULL",
                 "RoomID INT UNSIGNED NOT NULL");
@@ -138,14 +147,14 @@ namespace StudyPlatformSQLSetup
         public static void SetupAdmin(string externalConnectionString)
         {
             if (externalConnectionString != null)
-                connectionString = externalConnectionString;
+                _connectionString = externalConnectionString;
 
             WriteSetupMessage("Setting up admin person");
             Creator.CreateSecretary("Admin", "admin", "password");
         }
-        static void ExecuteQuery(string query)
+        private static void ExecuteQuery(string query)
         {
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
             using (MySqlCommand command = connection.CreateCommand())
             {
                 connection.Open();
@@ -154,7 +163,7 @@ namespace StudyPlatformSQLSetup
                 // connection.Close();
             }
         }
-        static void CreateTable(string tableName, params string[] variables)
+        private static void CreateTable(string tableName, params string[] variables)
         {
             WriteSetupMessage("Creating table for " + tableName);
             string query = "CREATE TABLE " + tableName + " (";
@@ -168,13 +177,13 @@ namespace StudyPlatformSQLSetup
             query += ");";
             ExecuteQuery(query);
         }
-        static void WriteSetupMessage(string message)
+        private static void WriteSetupMessage(string message)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("> " + message);
             Console.ResetColor();
         }
-        static void WriteErrorMessage(string message)
+        private static void WriteErrorMessage(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
