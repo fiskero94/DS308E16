901a1f9160b2915d697c2035d1eece09708ddabf
hype77 <bjma15@student.aau.dk>
2016-11-29 13:03:49 +0100

    assignment

--- StudyPlatformMVC/StudyPlatformMVC/Controllers/AssignmentsController.cs
+++ StudyPlatformMVC/StudyPlatformMVC/Controllers/AssignmentsController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StudyPlatformMVC.Models;
 
 namespace StudyPlatformMVC.Controllers
 {
     public class AssignmentsController : Controller
     {
         // GET: Assignments
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
-            return View();
+            List<Assignment> assignment = Student.GetByID(Convert.ToUInt32(id)).Assignments;
+            
+            return View("assignments", assignment);
         }
     }
 }
