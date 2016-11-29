bbad31d6eda85b2f6bfc5b620d89107505a0f0c3
kimx4560 <kimhtxt12@gmail.com>
2016-11-29 12:35:49 +0100

    Schedule

--- StudyPlatformMVC/StudyPlatformMVC/Controllers/ScheduleController.cs
+++ StudyPlatformMVC/StudyPlatformMVC/Controllers/ScheduleController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using StudyPlatformMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,57 @@ namespace StudyPlatformMVC.Controllers
 {
     public class ScheduleController : Controller
     {
-        // GET: Schedule
-        public ActionResult Index()
+
+
+        Func<DateTime, int> weekProjector = d => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(d, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Sunday);
+
+        [Route("Schedule/Index/{userID}/{weeknumber}")]
+        public ActionResult Index(int userID, int weeknumber)
         {
+
+            Person person = Person.GetByID(Convert.ToUInt32(userID));
+
+            if (person is Student)
+            {
+                List<Course> Courses = Student.GetByID(Convert.ToUInt32(userID)).Courses;
+                List<Lesson> Lessons = new List<Lesson>();
+
+                foreach (Course course in Courses)
+                {
+                    Lessons.AddRange(course.Lessons);
+                }
+
+                var lessonsByWeek = from lesson in Lessons group lesson by weekProjector(lesson.DateTime);
+
+                var weeklessons = lessonsByWeek.Where(x => x.Key == weeknumber);
+
+                var list = weeklessons.SelectMany(x => x).ToList();
+
+
+                return View(list);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            else if(person is Teacher)
+            {
+
+
+            }
+            else
+            {
+
+            }
+
             return View();
         }
     }
---
+++ StudyPlatformMVC/StudyPlatformMVC/Models/ScheduleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudyPlatformMVC.Models
+{
+    public class ScheduleViewModel
+    {
+
+        public Person Person { get; set; }
+
+        
+
+
+    }
+}
