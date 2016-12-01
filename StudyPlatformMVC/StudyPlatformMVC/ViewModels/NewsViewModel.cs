2ce80b814a2ac6e7c154f7368a353500ff6dc3f5
Rasmus Thomsen <Rasmuskjettrup@gmail.com>
2016-12-01 11:03:20 +0100

    News updated

---
+++ StudyPlatformMVC/StudyPlatformMVC/ViewModels/NewsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudyPlatformMVC.Models;
+
+namespace StudyPlatformMVC.ViewModels
+{
+    public class NewsViewModel
+    {
+        public List<News> News;
+        public News CurrentNews;
+    }
+}
