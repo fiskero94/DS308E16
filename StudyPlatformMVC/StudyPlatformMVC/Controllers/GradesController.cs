82a8a3ebe117ef4fee4d2625b7fb078ba6cedab0
Naver28 <Dlolholm@gmail.com>
2016-12-01 15:56:49 +0100

    Grade

--- StudyPlatformMVC/StudyPlatformMVC/Controllers/GradesController.cs
+++ StudyPlatformMVC/StudyPlatformMVC/Controllers/GradesController.cs
@@ -6,21 +6,51 @@ using System.Web.Mvc;
 using StudyPlatformMVC.Models;
 using StudyPlatformMVC.Database;
 using StudyPlatformMVC.Exceptions;
+using StudyPlatformMVC.ViewModels;
 
 namespace StudyPlatformMVC.Controllers
 {
     public class GradesController : Controller
     {
-        // GET: Grades
+        [Route("Grades/Index")]
         public ActionResult Index()
         {
+            //Test
+            Session["user"] = Student.GetByConditions("name='Iver Clausen'").Single();
+            //Actual
             Person person = (Person)Session["user"];
-            if((Person)Session["user"] is Student)
+            if(person == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            if(person is Student)
             {
                 List<CourseGrade> grades = CourseGrade.GetByConditions("StudentID=" + person.ID);
-                return View(grades);
+                return View(new CourseGradeViewModel { Grades = grades });
             }
-            return View(CourseGrade.GetAll());
+            return RedirectToAction("Index", "News");
+        }
+        
+        [Route("Grades/Index/{id}")]
+        public ActionResult Index(int id)
+        {
+            //Test
+            Session["user"] = Student.GetByConditions("name='Iver Clausen'").Single();
+            //Actual
+            Person person = (Person)Session["user"];
+            CourseGrade currentGrade;
+            if (person == null)
+            {
+                try
+                {
+                    currentGrade = CourseGrade.GetByID(Convert.ToUInt32(id));
+                }
+                catch(InvalidIDException)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return RedirectToAction("Index", "News");
         }
     }
 }
