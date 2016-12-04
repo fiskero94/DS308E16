5db17913b10942c975310ca0e14318b8cd53d888
kimx4560 <kimhtxt12@gmail.com>
2016-12-04 15:32:39 +0100

    Schedule

--- StudyPlatformMVC/StudyPlatformMVC/App_Start/BundleConfig.cs
+++ StudyPlatformMVC/StudyPlatformMVC/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace StudyPlatformMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/bootstrap-lumen-font.css",
                       "~/Content/navigation.css",
                       "~/Content/simple-sidebar.css",                                                    
