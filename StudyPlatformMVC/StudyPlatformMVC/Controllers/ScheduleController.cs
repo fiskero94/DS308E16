98241a32531fc37ab0e3c9db62f26c0266154355
kimx4560 <kimhtxt12@gmail.com>
2016-12-04 23:40:33 +0100

    Schedule

--- StudyPlatformMVC/StudyPlatformMVC/Controllers/ScheduleController.cs
+++ StudyPlatformMVC/StudyPlatformMVC/Controllers/ScheduleController.cs
@@ -13,58 +13,53 @@ namespace StudyPlatformMVC.Controllers
     {
 
         [Route("Schedule/Index/{userID}/{weeknumber}")]
-        public ActionResult Index(int userID, int weeknumber)
+        public ActionResult Index(int userID, int weekNumber)
         {
             Person person = Person.GetByID(Convert.ToUInt32(userID));
 
             if (person is Student)
             {
-                List<Course> Courses = Student.GetByID(Convert.ToUInt32(userID)).Courses;
-                List<Lesson> Lessons = new List<Lesson>();
-
-                foreach (Course course in Courses)
-                {
-                    foreach (Lesson lesson in course.Lessons)
-                    {
-                        
-
-
-                        DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
-                        Calendar cal = dfi.Calendar;
-
-                        if (cal.GetWeekOfYear(lesson.DateTime, dfi.CalendarWeekRule, dfi.FirstDayOfWeek) == weeknumber)
-                        {
-                            Lessons.Add(lesson);
-                        }  
-                    }
-                }
-
-                // mANGLER SORT BY TIMEOFDAY så alle kl8 lektioner kommer først.
-                var list = Lessons.OrderBy(x => x.DateTime.TimeOfDay).ToList();
-
-
-
-
-
-                return View(Lessons);
+                List<Course> courses = Student.GetByID(Convert.ToUInt32(userID)).Courses;
+                return View(FindAndSortLessonsForPerson(courses, weekNumber));
             }
-
-
-
-
-
-
             else if(person is Teacher)
             {
-
-
+                List<Course> courses = Teacher.GetByID(Convert.ToUInt32(userID)).Courses;
+                return View(FindAndSortLessonsForPerson(courses, weekNumber));
             }
-            else
+            else // Secretary
             {
+                return View();
+            }
+        }
 
+
+        private List<Lesson> FindAndSortLessonsForPerson(List<Course> courses, int weekNumber)
+        {
+            List<Lesson> lessons = new List<Lesson>();
+
+            foreach (Course course in courses)
+            {
+                foreach (Lesson lesson in course.Lessons)
+                {
+                    DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+                    Calendar cal = dfi.Calendar;
+
+                    if (cal.GetWeekOfYear(lesson.DateTime, dfi.CalendarWeekRule, dfi.FirstDayOfWeek) == weekNumber)
+                    {
+                        lessons.Add(lesson);
+                    }
+                }
             }
 
-            return View();
+            // mANGLER SORT BY TIMEOFDAY så alle kl8 lektioner kommer først.
+            List<Lesson> sortedList = lessons.OrderBy(x => x.DateTime.TimeOfDay).ToList();
+
+            return sortedList;
         }
+
+
+
+
     }
 }
