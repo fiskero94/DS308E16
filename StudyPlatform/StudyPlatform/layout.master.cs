1b3ebb335ab193254ef57ad14dfb9b375a63149f
fiskero94 <fiskero94@gmail.com>
2016-12-13 07:03:22 +0100

    navigation

--- StudyPlatform/StudyPlatform/layout.master.cs
+++ StudyPlatform/StudyPlatform/layout.master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -20,93 +21,231 @@ namespace StudyPlatform
             if (Session["user"] == null)
                 Response.Redirect("Login.aspx");
             else if (Session["user"] is Student)
-                foreach (HyperLink link in GetStudentLinks())
-                    SidePanel.Controls.Add(link);
+                SetupSidePanelContentsTable(StudentLinks);
             else if (Session["user"] is Teacher)
-                foreach (HyperLink link in GetTeacherLinks())
-                    SidePanel.Controls.Add(link);
-            else foreach (HyperLink link in GetSecretaryLinks())
-                SidePanel.Controls.Add(link);
-            NameLabel.Text = ((Person) Session["user"]).Name;
+                SetupSidePanelContentsTable(TeacherLinks);
+            else SetupSidePanelContentsTable(SecretaryLinks);
+            NameLabel.Text = ((Person)Session["user"]).Name;
         }
-
-
-        private void CreateStudentSidePanel()
+        private void SetupSidePanelContentsTable(IEnumerable<Pair> links)
         {
-            foreach (HyperLink link in GetStudentLinks())
+            foreach (Pair link in links)
             {
-                if (link.NavigateUrl == "/skema.aspx")
+                switch ((string)link.Second)
                 {
-                    
-                }
-                else if (link.NavigateUrl == "/afleveringer.aspx")
-                {
-
-                }
-                else if (link.NavigateUrl == "/indbakke.aspx")
-                {
-
-                }
-                else if (link.NavigateUrl == "/udbakke.aspx")
-                {
-
-                }
-                else
-                {
-                    SidePanel.Controls.Add(link);
+                    case "Nyheder": AddExpandRow(link, NewsExpandTable); break;
+                    case "Skema": AddExpandRow(link, ScheduleExpandTable); break;
+                    case "Afleveringer": AddExpandRow(link, AssignmentsExpandTable); break;
+                    case "Indbakke": AddExpandRow(link, RecievedMessagesExpandTable); break;
+                    case "Udbakke": AddExpandRow(link, SentMessagesExpandTable); break;
+                    default: AddGenericRow(link); break;
                 }
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        private static IEnumerable<HyperLink> GetStudentLinks()
+        private void AddExpandRow(Pair link, Table expandTable)
         {
-            return new List<HyperLink>
-            {
-                new HyperLink { NavigateUrl = "/nyheder.aspx", Text = "Nyheder" },
-                new HyperLink { NavigateUrl = "/skema.aspx", Text = "Skema" },
-                new HyperLink { NavigateUrl = "/kurser.aspx", Text = "Kurser" },
-                new HyperLink { NavigateUrl = "/afleveringer.aspx", Text = "Afleveringer" },
-                new HyperLink { NavigateUrl = "/karakterer.aspx", Text = "Karakterer" },
-                new HyperLink { NavigateUrl = "/fravaer.aspx", Text = "Fravær" },
-                new HyperLink { NavigateUrl = "/indbakke.aspx", Text = "Indbakke" },
-                new HyperLink { NavigateUrl = "/udbakke.aspx", Text = "Udbakke" }
-            };
+            TableRow row = new TableRow();
+            TableCell linkCell = new TableCell { Text = (string)link.Second };
+            linkCell.Attributes.Add("class", "clickable sidepanel-table-linkcell");
+            linkCell.Attributes.Add("onclick", "window.document.location='" + (string)link.First + "';");
+            if (Request.RawUrl.Contains((string)link.First)) // Should probably only request rawurl once
+                linkCell.Attributes.Add("style", "background: #75CAEB;");
+            TableCell expandCell = new TableCell();
+            expandCell.Attributes.Add("class", "clickable sidepanel-table-expandcell");
+            expandCell.Attributes.Add("data-toggle", "collapse");
+            expandCell.Attributes.Add("data-target", "#" + (string)link.Second + "accordion");
+            HtmlGenericControl expandIcon = new HtmlGenericControl("i");
+            expandIcon.Attributes.Add("class", "fa fa-chevron-down fa-2x");
+            expandCell.Controls.Add(expandIcon);
+            row.Cells.Add(linkCell);
+            row.Cells.Add(expandCell);
+            SidePanelContentsTable.Rows.Add(row);
+            TableRow expandRow = new TableRow();
+            TableCell expandRowContents = new TableCell { ColumnSpan = 2 };
+            var accordion = new HtmlGenericControl("div");
+            accordion.Attributes.Add("class", "collapse");
+            accordion.Attributes.Add("id", (string)link.Second + "accordion");
+            accordion.Controls.Add(expandTable);
+            expandRowContents.Controls.Add(accordion);
+            expandRow.Cells.Add(expandRowContents);
+            SidePanelContentsTable.Rows.Add(expandRow);
         }
-        private static IEnumerable<HyperLink> GetTeacherLinks()
+        private void AddGenericRow(Pair link)
         {
-            return new List<HyperLink>
-            {
-                new HyperLink { NavigateUrl = "/nyheder.aspx", Text = "Nyheder" },
-                new HyperLink { NavigateUrl = "/skema.aspx", Text = "Skema" },
-                new HyperLink { NavigateUrl = "/kurser.aspx", Text = "Kurser" },
-                new HyperLink { NavigateUrl = "/indbakke.aspx", Text = "Indbakke" },
-                new HyperLink { NavigateUrl = "/udbakke.aspx", Text = "Udbakke" }
-            };
+            TableRow row = new TableRow();
+            TableCell cell = new TableCell { Text = (string)link.Second };
+            cell.Attributes.Add("class", "clickable sidepanel-table-linkcell");
+            cell.Attributes.Add("onclick", "window.document.location='" + (string)link.First + "';");
+            if (Request.RawUrl.Contains((string)link.First)) // Should probably only request rawurl once
+                cell.Attributes.Add("style", "background: #75CAEB;");
+            row.Cells.Add(cell);
+            SidePanelContentsTable.Rows.Add(row);
         }
-        private static IEnumerable<HyperLink> GetSecretaryLinks()
+        private static Table NewsExpandTable
         {
-            return new List<HyperLink>
+            get
             {
-                new HyperLink { NavigateUrl = "/nyheder.aspx", Text = "Nyheder" },
-                new HyperLink { NavigateUrl = "/kurser.aspx", Text = "Kurser" },
-                new HyperLink { NavigateUrl = "/brugere.aspx", Text = "Brugere" },
-                new HyperLink { NavigateUrl = "/indbakke.aspx", Text = "Indbakke" },
-                new HyperLink { NavigateUrl = "/udbakke.aspx", Text = "Udbakke" }
-            };
+                Table table = new Table();
+                table.Attributes.Add("class", "sidepanel-table-inner table table-striped table-hover table-bordered");
+                foreach (News news in News.GetAll().OrderByDescending(o => o.DateTimePublished)
+                                                   .Take(3))
+                {
+                    TableRow row = new TableRow();
+                    row.Attributes.Add("class", "clickable");
+                    row.Attributes.Add("onclick", "window.document.location='/nyheder.aspx';");
+                    row.Cells.Add(new TableCell { Text = news.Title });
+                    row.Cells.Add(new TableCell { Text = GetTimeSince(news.DateTimePublished) });
+                    row.Cells[0].Attributes.Add("class", "col-sm-8");
+                    row.Cells[1].Attributes.Add("class", "col-sm-4");
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
         }
+        private Table ScheduleExpandTable
+        {
+            get
+            {
+                Table table = new Table();
+                table.Attributes.Add("class", "sidepanel-table-inner table table-striped table-hover table-bordered");
+                List<Course> courses = new List<Course>();
+                if (Session["user"] is Student)
+                    courses = ((Student) Session["user"]).Courses;
+                else if (Session["user"] is Teacher)
+                    courses = ((Teacher) Session["user"]).Courses;
+                List<Lesson> lessons = new List<Lesson>();
+                foreach (Course course in courses)
+                    lessons.AddRange(course.Lessons);
+                foreach (Lesson lesson in lessons.Where(l => l.DateTime > DateTime.Now)
+                                                 .OrderBy(o => o.DateTime)
+                                                 .Take(3))
+                {
+                    TableRow row = new TableRow();
+                    row.Attributes.Add("class", "clickable");
+                    row.Attributes.Add("onclick", "window.document.location='/skema.aspx';");
+                    row.Cells.Add(new TableCell { Text = lesson.Course.Name });
+                    row.Cells.Add(new TableCell { Text = GetTimeUntil(lesson.DateTime) });
+                    row.Cells[0].Attributes.Add("class", "col-sm-8");
+                    row.Cells[1].Attributes.Add("class", "col-sm-4");
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
+        }
+        private Table AssignmentsExpandTable
+        {
+            get
+            {
+                Table table = new Table();
+                table.Attributes.Add("class", "sidepanel-table-inner table table-striped table-hover table-bordered");
+                List<AssignmentDescription> assignmentDescriptions = new List<AssignmentDescription>();
+                foreach (Course course in ((Student)Session["user"]).Courses)
+                    assignmentDescriptions.AddRange(course.AssignmentDescriptions);
+                foreach (AssignmentDescription assignmentDescription in
+                         assignmentDescriptions.Where(ad => ad.HasExpired == false && 
+                                                      ad.Assignments.All(a => a.Student.ID != ((Student) Session["user"]).ID))
+                                               .OrderBy(o => o.Deadline)
+                                               .Take(3))
+                {
+                    TableRow row = new TableRow();
+                    row.Attributes.Add("class", "clickable");
+                    row.Attributes.Add("onclick", "window.document.location='/afleveringer.aspx';");
+                    row.Cells.Add(new TableCell { Text = assignmentDescription.Title });
+                    row.Cells.Add(new TableCell { Text = GetTimeUntil(assignmentDescription.Deadline) });
+                    row.Cells[0].Attributes.Add("class", "col-sm-8");
+                    row.Cells[1].Attributes.Add("class", "col-sm-4");
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
+        }
+        private Table RecievedMessagesExpandTable
+        {
+            get
+            {
+                Table table = new Table();
+                table.Attributes.Add("class", "sidepanel-table-inner table table-striped table-hover table-bordered");
+                foreach (Message message in ((Person)Session["user"]).RecievedMessages
+                         .OrderByDescending(o => o.DateTimeSent).Take(3))
+                {
+                    TableRow row = new TableRow();
+                    row.Attributes.Add("class", "clickable");
+                    row.Attributes.Add("onclick", "window.document.location='/indbakke.aspx';");
+                    row.Cells.Add(new TableCell { Text = message.Title });
+                    row.Cells.Add(new TableCell { Text = GetTimeSince(message.DateTimeSent) });
+                    row.Cells[0].Attributes.Add("class", "col-sm-8");
+                    row.Cells[1].Attributes.Add("class", "col-sm-4");
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
+        }
+        private Table SentMessagesExpandTable
+        {
+            get
+            {
+                Table table = new Table();
+                table.Attributes.Add("class", "sidepanel-table-inner table table-striped table-hover table-bordered");
+                foreach (Message message in ((Person)Session["user"]).SentMessages
+                         .OrderByDescending(o => o.DateTimeSent).Take(3))
+                {
+                    TableRow row = new TableRow();
+                    row.Attributes.Add("class", "clickable");
+                    row.Attributes.Add("onclick", "window.document.location='/udbakke.aspx';");
+                    row.Cells.Add(new TableCell { Text = message.Title });
+                    row.Cells.Add(new TableCell { Text = GetTimeSince(message.DateTimeSent) });
+                    row.Cells[0].Attributes.Add("class", "col-sm-8");
+                    row.Cells[1].Attributes.Add("class", "col-sm-4");
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
+        }
+        private static string GetTimeUntil(DateTime dateTime)
+        {
+            TimeSpan timeUntil = dateTime.Subtract(DateTime.Now);
+            if (timeUntil.TotalDays >= 1)
+                return "om " + (int)timeUntil.TotalDays + " dage";
+            if (timeUntil.TotalHours >= 1)
+                return "om " + (int)timeUntil.TotalHours + " timer";
+            return "om " + (int)timeUntil.TotalMinutes + " minutter";
+        }
+        private static string GetTimeSince(DateTime dateTime)
+        {
+            TimeSpan timeSince = DateTime.Now.Subtract(dateTime);
+            if (timeSince.TotalDays >= 1)
+                return (int)timeSince.TotalDays + " dage siden";
+            if (timeSince.TotalHours >= 1)
+                return (int)timeSince.TotalHours + " timer siden";
+            return (int)timeSince.TotalMinutes + " minutter siden";
+        }
+        private static readonly List<Pair> StudentLinks = new List<Pair>
+        {
+            new Pair("/nyheder.aspx", "Nyheder"),
+            new Pair("/skema.aspx", "Skema"),
+            new Pair("/kurser.aspx", "Kurser"),
+            new Pair("/afleveringer.aspx", "Afleveringer"),
+            new Pair("/karakterer.aspx", "Karakterer"),
+            new Pair("/fravaer.aspx", "Fravær"),
+            new Pair("/indbakke.aspx", "Indbakke"),
+            new Pair("/udbakke.aspx", "Udbakke")
+        };
+        private static readonly List<Pair> TeacherLinks = new List<Pair>
+        {
+            new Pair("/nyheder.aspx", "Nyheder"),
+            new Pair("/skema.aspx", "Skema"),
+            new Pair("/kurser.aspx", "Kurser"),
+            new Pair("/indbakke.aspx", "Indbakke"),
+            new Pair("/udbakke.aspx", "Udbakke")
+        };
+        private static readonly List<Pair> SecretaryLinks = new List<Pair>
+        {
+            new Pair("/nyheder.aspx", "Nyheder"),
+            new Pair("/kurser.aspx", "Kurser"),
+            new Pair("/brugere.aspx", "Brugere"),
+            new Pair("/indbakke.aspx", "Indbakke"),
+            new Pair("/udbakke.aspx", "Udbakke")
+            
+        };
     }
 }
--- StudyPlatform/StudyPlatform/layout.master.designer.cs
+++ StudyPlatform/StudyPlatform/layout.master.designer.cs
@@ -31,6 +31,15 @@ namespace StudyPlatform {
         protected global::System.Web.UI.WebControls.Panel SidePanel;
         
         /// <summary>
+        /// SidePanelContentsTable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table SidePanelContentsTable;
+        
+        /// <summary>
         /// TitelLabel control.
         /// </summary>
         /// <remarks>
